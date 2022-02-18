24e1cb9a8a0b9421371beacc3d9b85275f4ba220
Veronika Mitichashvili <nika.mitichashili@gmail.com>
2022-02-18 18:59:01 +0400

    deleted useless stuff + added gitignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { Todo } from './components/Todo';
+import "./App.css";
+import { Todo } from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-     <Todo/>
+      <Todo />
     </div>
   );
 }
